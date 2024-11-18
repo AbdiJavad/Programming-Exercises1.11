2eb6de4d4f487af21c18dc29dac8bd367d15c0e8
JavadAbdi <Mirjavadabdi@gmail.com>
2024-11-18 22:59:22 +0330

    Updated,Changed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
-
-
+    
     public static void main(String[] args) {
         System.out.println("Population projection");
         System.out.println("Current population: 312032486");
